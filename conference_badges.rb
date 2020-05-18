dde7ec34dc4c3691a5f59b58fa55ac0667068224
jassmine tatum <jassmine.tatum@gmail.com>
2020-05-18 02:13:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,27 +1,29 @@
 def badge_maker(name)
-  "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array_of_name)
-  array_of_name.each do |name|
-    badges = ["Hello, my name is #{name}"]
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+    badges.push(badge_maker(name))
   end
-  badges
+  return badges
 end
 
-def assign_rooms(speakers)
-  arr = []
-  speakers.each_with_index {|name, index|
-    arr[index] = "Hello, #{name}! You'll be assigned to room #{index+1}!"
-  }
-  arr
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index do |name, number|
+    room_assignments.push("Hello, #{name}! You'll be assigned to room #{number+1}!")
+  end
+  room_assignments
 end
 
-def printer(name)
-  batch_badge_creator(name).each do |badge|
-    puts badge
-  end
-  assign_rooms(name).each do |badge|
-    puts badge
-  end
+def printer(names)
+  badge = batch_badge_creator(names)
+  schedule = assign_rooms(names)
+
+  badge.each {|bad| puts bad}
+  schedule.each {|assignment| puts assignment}
 end
